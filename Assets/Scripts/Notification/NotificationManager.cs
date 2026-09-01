7cd92ba9284d4e103023a2a613441c251feae334
PixelPerfect <mamdouhatef222@gmail.com>
2026-09-01 08:42:52 +0300

    NotificationManager is now sealed

--- Assets/Scripts/Notification/NotificationManager.cs
+++ Assets/Scripts/Notification/NotificationManager.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class NotificationManager : MonoBehaviour
+public sealed class NotificationManager : MonoBehaviour
 {
     [SerializeField] private TMPro.TextMeshProUGUI titleText, descriptionText;
 
