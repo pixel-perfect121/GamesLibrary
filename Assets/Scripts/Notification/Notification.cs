601767e6b1300dba77087f453e1b88af6391ba82
PixelPerfect <mamdouhatef222@gmail.com>
2026-09-01 07:54:56 +0300

    Notifications has entered the chat

---
+++ Assets/Scripts/Notification/Notification.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+/// <summary>Game-wide notification system.</summary>
+/// <remarks>Useful for spamming FIX BEDROCK MOJANG 🗿.</remarks>
+public readonly struct Notification
+{
+    /// <summary>Title of subject or name of sender.</summary>
+    public readonly string title;
+    /// <summary>Extra information that is carried with the object.</summary>
+    public readonly string description;
+    /// <summary>The date at which this object was created at.</summary>
+    public readonly DateTime creationTime;
+
+    /// <summary>Event carrying information about the object.</summary>
+    public static event Action<Notification> Created;
+
+    /// <summary>Create and invoke Created event.</summary>
+    /// <param name="title">Title of subject or name of sender.</param>
+    /// <param name="description">Extra information.</param>
+    /// <param name="priority">How important is this notification.</param>
+    public Notification(string title, string description)
+    {
+        this.title = title; this.description = description;
+        creationTime = DateTime.Now;
+
+        Created?.Invoke(this);
+    }
+}
---
+++ Assets/Scripts/Notification/NotificationManager.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class NotificationManager : MonoBehaviour
+{
+    [SerializeField] private TMPro.TextMeshProUGUI titleText, descriptionText;
+
+    private Animator animator;
+    private bool isBusy;
+    private readonly int Animate = Animator.StringToHash("Animate");
+    private readonly System.Collections.Generic.Queue<Notification> queue = new();
+
+    void Awake() { animator = GetComponent<Animator>(); }
+
+    private void Notify(Notification notification)
+    {
+        if (titleText == null || descriptionText == null) return;
+        if (isBusy) { queue.Enqueue(notification); return; }
+
+        isBusy = true;
+
+        titleText.text = notification.title;
+        descriptionText.text = notification.description;
+
+        if (animator != null)
+        {
+            animator.ResetTrigger(Animate);
+            animator.SetTrigger(Animate);
+        }
+    }
+
+    public void NotifyNext()
+    {
+        isBusy = false;
+
+        if (queue.Count > 0) Notify(queue.Dequeue());
+    }
+
+    void OnEnable() { Notification.Created += Notify; }
+    void OnDisable() { Notification.Created -= Notify; }
+}
