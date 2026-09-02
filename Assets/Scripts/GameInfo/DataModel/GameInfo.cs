b2351456eea799e6cf4fd9200f2ac19ba5038ae2
PixelPerfect <mamdouhatef222@gmail.com>
2026-09-02 05:52:58 +0300

    Extended GameInfo system for flexibility
    Future complexity are soon to arrive, but let's get core mechanics in place first.

--- Assets/Scripts/GameInfo/DataModel/GameInfo.cs
+++ Assets/Scripts/GameInfo/DataModel/GameInfo.cs
@@ -1,17 +1,24 @@
-﻿/// <summary>Struct holding information about a given game.</summary>
-[System.Serializable]
+﻿using System;
+
+/// <summary>Type of modification done.</summary>
+public enum Modification { Description, Rating, Both }
+
+/// <summary>Struct holding information about a given game.</summary>
+[Serializable]
 public struct GameInfo
 {
     /// <summary>Name or title of the game.</summary>
-    public readonly string title;
+    public readonly string Title { get; }
     /// <summary>Extra information about the game.</summary>
-    public string description;
-    /// <summary>How much you liked the game. <br></br>
-    /// Idealy, this should goes from 0 to 5, but don't let this limit you 🗿.</summary>
-    public int rating;
+    public string Description { get; private set; }
+    /// <summary>How much you liked the game.</summary>
+    /// <remarks>Idealy, this should goes from 0 to 5, but don't let this limit you 🗿.</remarks>
+    public int Rating { get; private set; }
 
-    /// <summary>Event that fired when creating an instance of this object.</summary>
-    public static event System.Action<GameInfo> Created;
+    /// <summary>Fires when creating an instance of this object.</summary>
+    public static event Action<GameInfo> Created;
+    /// <summary>Fires when using any modify method.</summary>
+    public static event Action<GameInfo, Modification> Modified;
 
     /// <summary>Create and invoke Created event.</summary>
     /// <param name="name">Name or title of the game.</param>
@@ -19,12 +26,30 @@ public struct GameInfo
     /// <param name="rating">How much you liked the game.</param>
     public GameInfo(string name, string description, int rating)
     {
-        this.title = name; this.description = description; this.rating = rating;
-        // Modification of description and rating will come later
+        Title = name; Description = description; Rating = rating;
         Created?.Invoke(this);
     }
 
-    /// <summary>Gives the ID of the GameInfo object.</summary>
-    /// <returns>returns an ID for the game formatted as lower_case_game</returns>
-    public readonly string GetID() => $"{title.ToLowerInvariant().Replace(" ", "_")}_game";
+    /// <summary>Change description and invoke Modified event.</summary>
+    public void Modify(string newDescription)
+    {
+        Description = newDescription; // Maybe store old data and send it via the event
+        Modified?.Invoke(this, Modification.Description);
+    }
+    /// <summary>Change rating and invoke Modified event.</summary>
+    public void Modify(int newRating)
+    {
+        Rating = newRating;
+        Modified?.Invoke(this, Modification.Rating);
+    }
+    /// <summary>Change description, rating, and invoke Modified event.</summary>
+    public void Modify(string newDescription, int newRating)
+    {
+        Description = newDescription; Rating = newRating;
+        Modified?.Invoke(this, Modification.Both);
+    }
+
+    /// <summary>Gives a string associated with the object.</summary>
+    /// <returns>returns an ID for the game, formatted as lower_case_game</returns>
+    public readonly string GetID() => $"{Title.ToLowerInvariant().Replace(" ", "_")}_game";
 }
--- Assets/Scripts/GameInfo/GameInfoManager.cs
+++ Assets/Scripts/GameInfo/GameInfoManager.cs
@@ -6,10 +6,23 @@ public static class GameInfoManager
     private static void Initialize()
     {
         GameInfo.Created += OnGameInfoCreated;
+        GameInfo.Modified += OnGameInfoModified;
     }
 
     private static void OnGameInfoCreated(GameInfo gameInfo)
     {
-        
+        new Notification("Game added", $"{gameInfo.Title} has been added");
+    }
+    private static void OnGameInfoModified(GameInfo gameInfo, Modification modification)
+    {
+        string message = modification switch
+        {
+            Modification.Description => "\'s description has been modified",
+            Modification.Rating => "\'s rating has been modified",
+            Modification.Both => "has received a treatment for baldness",
+            _ => "has remained the same. Somethings are just better this way"
+        };
+
+        new Notification("Game modified", $"{gameInfo.Title} {message}");
     }
 }
