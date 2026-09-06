94e1418ac42c1d0451f02b9fa0f370ecd1d822c2
PixelPerfect <mamdouhatef222@gmail.com>
2026-09-06 20:34:29 +0300

    NOTIFICATIONS AGAIN 🗿🗿🗿

---
+++ Assets/Scripts/Notification/Notification.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+/// <summary>Notifications across the game, useful for sending FIX BEDROCK MOJANG 🗿🗿🗿.</summary>
+public struct Notification
+{
+    /// <summary>Subject's title or name of sender.</summary>
+    public readonly string title;
+    /// <summary>Content that will be sent, *cough* FIX BEDROCK MOJANG *cough*.</summary>
+    public readonly string description;
+    /// <summary>Time at which this object was created.</summary>
+    public readonly DateTime creationTime;
+    /// <summary>Time at which this object was delivered.</summary>
+    public DateTime DeliveryTime { get; private set; }
+
+    /// <summary>Automatically invoked when creating new notification object.</summary>
+    public static event Action<Notification> Created;
+
+    /// <summary>Create and invokes Created event.</summary>
+    /// <param name="title">Name of sender or subject.</param>
+    /// <param name="message">Content that will be displayed under title.</param>
+    public Notification(string title, string message)
+    {
+        this.title = title; this.description = message;
+        creationTime = DateTime.UtcNow; DeliveryTime = new();
+
+        Created?.Invoke(this);
+    }
+
+    public void SetDeliveryTime(DateTime deliveryTime) => DeliveryTime = deliveryTime;
+}
---
+++ Assets/Scripts/Notification/NotificationManager.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public sealed class NotificationManager : MonoBehaviour
+{
+    [SerializeField] private TMPro.TextMeshProUGUI titleText, descriptionText, deliveryTimeText;
+
+    private Animator animator;
+    private readonly int AnimateHash = Animator.StringToHash("Animate");
+    private readonly System.Collections.Generic.Queue<Notification> notificationQueue = new();
+    private bool isBusy;
+
+    void Awake() { animator = GetComponent<Animator>(); }
+
+    private void Notify(Notification notification)
+    {
+        if (animator == null || titleText == null || descriptionText == null || deliveryTimeText == null) return;
+        if (isBusy) { notificationQueue.Enqueue(notification); return; }
+
+        isBusy = true;
+
+        notification.SetDeliveryTime(System.DateTime.UtcNow);
+
+        titleText.text = notification.title;
+        descriptionText.text = notification.description;
+        deliveryTimeText.text = notification.DeliveryTime.ToString("hh:mm(tt)");
+
+        animator.ResetTrigger(AnimateHash);
+        animator.SetTrigger(AnimateHash);
+    }
+
+    public void Finished()
+    {
+        isBusy = false;
+
+        if (notificationQueue.Count > 0) Notify(notificationQueue.Dequeue());
+    }
+
+    void OnEnable() { Notification.Created += Notify; }
+    void OnDisable() { Notification.Created -= Notify; }
+}
