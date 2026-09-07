fb15cfe64d649abb98316e7c1f668148f222586b
PixelPerfect <mamdouhatef222@gmail.com>
2026-09-07 22:43:02 +0300

    QOL

--- Assets/Scripts/Notification/Notification.cs
+++ Assets/Scripts/Notification/Notification.cs
@@ -1,26 +1,20 @@
 ﻿using System;
 
-/// <summary>Game-wide notifying system.</summary>
-public readonly struct Notification
+public struct Notification
 {
-    /// <summary>Title of subject or name of sender.</summary>
-    public readonly string title;
-    /// <summary>Extra information that is carried with the object.</summary>
-    public readonly string description;
-    /// <summary>The date at which this object was created at.</summary>
+    public readonly string title, description;
     public readonly DateTime creationTime;
+    public DateTime DeliveryTime { get; private set; }
 
-    /// <summary>Event carrying information about the object.</summary>
     public static event Action<Notification> Created;
 
-    /// <summary>Create and invoke Created event.</summary>
-    /// <param name="title">Title of subject or name of sender.</param>
-    /// <param name="description">Extra information.</param>
     public Notification(string title, string description)
     {
         this.title = title; this.description = description;
-        creationTime = DateTime.UtcNow;
+        creationTime = DateTime.UtcNow; DeliveryTime = new();
 
         Created?.Invoke(this);
     }
+
+    public void SetDeliveryTime(DateTime deliveryTime) => DeliveryTime = deliveryTime;
 }
--- Assets/Scripts/Notification/NotificationManager.cs
+++ Assets/Scripts/Notification/NotificationManager.cs
@@ -2,30 +2,29 @@ using UnityEngine;
 
 public sealed class NotificationManager : MonoBehaviour
 {
-    [SerializeField] private TMPro.TextMeshProUGUI titleText, descriptionText;
+    [SerializeField] private TMPro.TextMeshProUGUI titleText, descriptionText, deliveryTimeText;
 
     private Animator animator;
-    private bool isBusy;
     private readonly int Animate = Animator.StringToHash("Animate");
     private readonly System.Collections.Generic.Queue<Notification> queue = new();
+    private bool isBusy;
 
     void Awake() { animator = GetComponent<Animator>(); }
 
     private void Notify(Notification notification)
     {
-        if (titleText == null || descriptionText == null) return;
+        if (animator == null || titleText == null || descriptionText == null || deliveryTimeText == null) return;
         if (isBusy) { queue.Enqueue(notification); return; }
 
         isBusy = true;
 
+        notification.SetDeliveryTime(System.DateTime.UtcNow);
+
         titleText.text = notification.title;
         descriptionText.text = notification.description;
+        deliveryTimeText.text = $"Delivery time: {notification.DeliveryTime:hh:mm(tt)}";
 
-        if (animator != null)
-        {
-            animator.ResetTrigger(Animate);
-            animator.SetTrigger(Animate);
-        }
+        animator.ResetTrigger(Animate); animator.SetTrigger(Animate);
     }
 
     public void NotifyNext()
