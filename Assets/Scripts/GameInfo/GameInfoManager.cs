3f2db74878be262cca7f722c9e8e31f6d1d2e046
PixelPerfect <mamdouhatef222@gmail.com>
2026-09-10 17:21:12 +0300

    Update GameInfoManager.cs

--- Assets/Scripts/GameInfo/GameInfoManager.cs
+++ Assets/Scripts/GameInfo/GameInfoManager.cs
@@ -12,31 +12,34 @@ public static class GameInfoManager
         GameInfo.Modified += OnGameInfoModified;
     }
 
-    public static IReadOnlyList<GameInfo> RequestGameInfos() => SaveManager.GameData.GameInfoDictionary.Values.ToList();
-
     private static void OnGameInfoCreated(GameInfo gameInfo)
     {
-        if (SaveManager.GameData.GameInfoDictionary.ContainsKey(gameInfo.GetID()))
+        string key = gameInfo.GetID();
+        if (SaveManager.GameData.GameInfoDictionary.ContainsKey(key))
         {
-            new Notification("Game already exists", $"{gameInfo.Title} already exists in the database");
+            //new Notification("Game already exists", $"{gameInfo.Title} already exists in the database");
             return;
         }
 
-        SaveManager.GameData.GameInfoDictionary.Add(gameInfo.GetID(), gameInfo);
+        SaveManager.GameData.GameInfoDictionary.Add(key, gameInfo);
         SaveManager.RequestSave(Method.Async);
 
-        new Notification("Game added", $"{gameInfo.Title} has been added");
+        //new Notification("Game added", $"{gameInfo.Title} has been added");
     }
     private static void OnGameInfoModified(GameInfo gameInfo, Modification modification)
     {
-        if (!SaveManager.GameData.GameInfoDictionary.ContainsKey(gameInfo.GetID()))
+        string key = gameInfo.GetID();
+        if (!SaveManager.GameData.GameInfoDictionary.ContainsKey(key))
         {
-            new Notification("Game does not exist", "The given ID does not correspond to any game");
+            //new Notification("Game does not exist", "The given ID does not correspond to any game");
             return;
         }
 
+        SaveManager.GameData.GameInfoDictionary[key] = gameInfo;
         SaveManager.RequestSave(Method.Async);
 
-        new Notification("Game modified", $"{gameInfo.Title} has been modified");
+        //new Notification("Game modified", $"{gameInfo.Title} has been modified");
     }
+
+    public static IReadOnlyList<GameInfo> RequestGameInfos() => SaveManager.GameData.GameInfoDictionary.Values.ToList();
 }
